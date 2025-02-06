581b9e113dfe4516bffe0a78b2b51baf52d42121
Viktor Lukashchuk <lukashchuk.victor@gmail.com>
2025-02-06 21:27:13 +0200

    hw5

--- lesson5-functions/src/functions.js
+++ lesson5-functions/src/functions.js
@@ -1,23 +1,23 @@
 const numArr = [1, 2, 3];
 const strArr = ['1', 'test', 'true'];
 
-const arrFunc = function (arr) {
+function arrFunc(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum = sum + arr[i];
     }
     console.log(sum);
-};
+}
 
-const arrFunc2 = function (arr1, arr2) {
+function arrFunc2(arr1, arr2) {
     let unitedArr = [...arr1, ...arr2];
     arrFunc(unitedArr);
-};
+}
 
-const arrFunc3 = function (...arrs) {
+function arrFunc3(...arrs) {
     let unitedArr = [...arrs];
     arrFunc(unitedArr);
-};
+}
 // wanted to use it in order to receive one two or more arrs at the time but not able to do it properly without chatgpt(
 
 console.log('---------------------');
